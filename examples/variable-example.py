714704d9f44f9bc700bc429287433e4669459785
Alex McCaskey <mccaskeyaj@ornl.gov>
2018-09-21 20:39:07 +0000

    updating dwave example
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- examples/variable-example.py
+++ examples/variable-example.py
@@ -1,23 +1,44 @@
-import pyxacc as xacc
-
-src = """__qpu__ variable(AcceleratorBuffer ab, double ta, double tp, double tq, double h, double J) {
-    anneal ta tp tq;
-    0 0 h;
-    1 1 h;
-    0 1 J;
-}"""
-
+import xacc
 xacc.Initialize(['--compiler','dwave-qmi'])
 
-qpu = xacc.getAccelerator('dwave')
-qubits = qpu.createBuffer('qbits')
+dwave = xacc.getAccelerator('dwave')
+qubits = dwave.createBuffer('q')
 
-p = xacc.Program(qpu, src)
-p.build()
+#xacc.setOption('dwave-solver', 'DW_2000Q_2')
+# You can write your kernel like this...
+# src = """__qpu__ variable(AcceleratorBuffer ab, double ta, double tp, double tq, double h, double J) {
+#    anneal ta tp tq;
+#    0 0 h;
+#    1 1 h;
+#    0 1 J;
+# }"""
+#
+# Then use the Program/Kernel API
+#
+# p = xacc.Program(qpu, src)
+# p.build()
+#
+# variable = p.getKernel('variable')
 
-variable = p.getKernel('variable')
+# for t in [20.,30.]:
+#    variable.execute(qubits, [t, 0., 0., 2., 3.])
 
-for t in [20.,30.]:
-    variable.execute(qubits, [t, 0., 0., 2., 3.])
+# OR without annealing schedule
+@xacc.qpu(accelerator=dwave)
+def f(buffer, h, j):
+    qmi(0,0,h)
+    qmi(1,1,h)
+    qmi(0,1,j)
+
+# This is how you can 
+# set a custom embedding
+# qubits.addExtraInfo('embedding', {0:[0],1:[4]})
+
+# Execute on D-Wave
+f(qubits, 1., 2.)
+
+print(qubits)
+
+
 
 xacc.Finalize()
